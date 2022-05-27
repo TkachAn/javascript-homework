07cdef638ddd5b02999f66b43ca9855725500641
TkachAn <4ndrey.tkachenko@gmail.com>
2022-05-27 23:57:57 +0300

    Update task-08.js

--- v2/06/src/js/task-08.js
+++ v2/06/src/js/task-08.js
@@ -1,1 +1,26 @@
+const user = {};
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event){
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    // console.log(formData);
+    formData.forEach((value,name)=>{
+        // console.log('name:',name);
+        // console.log('value:',value); 
+        console.log(name,value);
+        if(!value || !name){
+            alert("Не все поля формы заполнены!");
+            return;
+          
+        }else{
+            // user = `${name}: ${value}`;
+          user = name;
+          user = value;
+            console.log(user); 
+            form.reset();
+        }
+         
+    })
+}
 
